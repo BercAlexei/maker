34f226a6a97f69e81236a03f00b42659046f28d3
Alexei <1996Leha@mail.ru>
2021-04-21 02:04:36 +0500

    start js file

--- src/js/_script.js
+++ src/js/_script.js
@@ -2,4 +2,15 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
+    let input = document.querySelector('input'),
+        span = document.querySelector('span'),
+        btn = document.querySelector('button');
+
+
+
+
+    btn.addEventListener('click', (e) => {
+        e.preventDefault();
+
+    })
 });
